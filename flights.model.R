34ff21e148b71351a99cb8e8e5fe5f6cee773506
Michael <michael@Michaels-MacBook-Pro.local>
2018-05-07 17:04:28 -0400

    Started using LogitBoost

--- flights.model.R
+++ flights.model.R
@@ -6,21 +6,20 @@ library(tidyverse)
 
 clean_flights <- clean.flights
 
-clean.flights <- sample_frac(clean_flights, 0.2)
+clean.flights <- sample_frac(clean_flights, 0.01)
 
 training.set <- clean.flights %>%
   filter(DATE < as_date("2009-12-31"))
+
 
 test.set <- clean.flights %>%
   filter(DATE >= as_date("2009-12-31"),
          DEST %in% unique(training.set$DEST),
          UNIQUE_CARRIER %in% unique(training.set$UNIQUE_CARRIER))
 
-our.formula <- "factor(CANCELLED) ~ SNOW.bos + SNOW_lag1.bos + SNOW_lag2.bos + SNOW_lag3.bos +
-                SNOW.nyc + SNOW_lag1.nyc + SNOW_lag2.nyc + SNOW_lag3.nyc + TMIN.bos + TMAX.bos + factor(MONTH)
-                + TMIN.nyc + TMAX.nyc + TMIN.atl + TMAX.atl + PRCP.bos + DISTANCE + DEST + UNIQUE_CARRIER +
-                DEST*UNIQUE_CARRIER + factor(CANCELLATION_CODE)"
+our.formula <- "factor(CANCELLED) ~ factor(CANCELLATION_CODE)"
 
+#Test Kappa >  Training Kappa because some cancelled flights don't have cancellation codes
 training.model <- glm(our.formula,
                       family = "binomial",
                       data = training.set)
@@ -33,20 +32,31 @@ training.set$pred <- stats::predict.glm(training.model,
 training.Kappa.val <- (fmsb::Kappa.test(table(training.set$pred > 0.5, training.set$CANCELLED)))$Result$estimate
 
 test.set$pred <- stats::predict.glm(training.model,
-                           test.set,
-                           type = "response")
+                                    test.set,
+                                    type = "response")
 
 Kappa.val <- (fmsb::Kappa.test(table(test.set$CANCELLED, test.set$pred > 0.5)))$Result$estimate
 
 
 mse.training.model <- mean((test.set$pred - test.set$CANCELLED)^2)
 
-
-
-alex <- train(factor(CANCELLED) ~ SNOW.bos,
+#Use ML to select variables ad get better predictive power
+#SNOW.bos + PRCP.bos + TMAX.bos + TMIN.bos + DATE + DISTANCE + UNIQUE_CARRIER
+print(Sys.time())
+alex <- train(factor(CANCELLED) ~ SNOW.bos + TMAX.bos + TMIN.bos + DATE + DISTANCE + UNIQUE_CARRIER + SNOW.nyc,
               method = "LogitBoost",
               metric = "Kappa",
               data = training.set)
 print(Sys.time())
+
+training.set$pred <- predict(alex,
+                             data = training.set)
+
+training.Kappa.val.ml <- (fmsb::Kappa.test(table(training.set$pred, training.set$CANCELLED)))$Result$estimate
+
+test.set$pred <- predict(alex,
+                         test.set)
+
+test.Kappa.val.ml <- (fmsb::Kappa.test(table(test.set$CANCELLED, test.set$pred)))$Result$estimate
 
 alex$results
